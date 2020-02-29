14efe15519d5583c0d90d74b0f9d74e2b844ef56
g-wizzy <pierre.burki@protonmail.com>
2020-02-29 19:45:05 +0100

    Adjust autostart.sh for Ubuntu

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Compton for transparency
-compton --config /home/pierre/.compton &
+#compton --config /home/pierre/.compton &
+compton &
 
 # Runs elecshot for screen capture
-/usr/local/lib/nodejs/bin/npm start --prefix /home/pierre/elecshot &
+# /usr/local/lib/nodejs/bin/npm start --prefix /home/pierre/elecshot &
 
 # Gnome screensaver allows to lock the screen at any time
-gnome-screensaver &
+# gnome-screensaver &
 
 # Network applet
 nm-applet &
