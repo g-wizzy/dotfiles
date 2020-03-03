6ffda3c1d21ab14ee85ba49e90bd0e6c2793b9ff
Pierre Bürki <pierre.burki@protonmail.com>
2020-03-03 09:14:55 +0100

    Clean old autostart, and add albert and compton

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -4,14 +4,11 @@
 #compton --config /home/pierre/.compton &
 compton &
 
-# Runs elecshot for screen capture
-# /usr/local/lib/nodejs/bin/npm start --prefix /home/pierre/elecshot &
-
-# Gnome screensaver allows to lock the screen at any time
-# gnome-screensaver &
-
 # Network applet
 nm-applet &
 
+# Albert launcher
+albert &
+
 # Cheap fix for unity hub
-~/Applications/UnityHub*.AppImage
+~/Applications/UnityHub*.AppImage &
