4dd7b4cf9b530a31148dc8cc6ab7661d0edd7ca3
Pierre Bürki <pierre.burki@he-arc.ch>
2020-12-07 15:01:09 +0100

    Clean up the install script

--- install.sh
+++ install.sh
@@ -12,24 +12,13 @@ ln -s $dir/profile ~/.profile
 ln -s $dir/zshrc ~/.zshrc
 
 # Install kitty
-mkdir -p ~/.config/kitty
-ln -s $dir/kitty/kitty.conf ~/.config/kitty/kitty.conf
-ln -s $dir/kitty/theme.conf ~/.config/kitty/theme.conf
+ln -s $dir/kitty ~/.config/kitty
 
 # Install starship
-curl -fsSL https://starship.rs/install.sh | bash
 ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
 
 # Install qtile config
-mkdir -p ~/.config/qtile
-ln -s $dir/qtile/config.py ~/.config/qtile/config.py
-ln -s $dir/qtile/hooks.py ~/.config/qtile/hooks.py
-ln -s $dir/qtile/groups.py ~/.config/qtile/groups.py
-ln -s $dir/qtile/keys.py ~/.config/qtile/keys.py
-ln -s $dir/qtile/layouts.py ~/.config/qtile/layouts.py
-ln -s $dir/qtile/screens.py ~/.config/qtile/screens.py
-ln -s $dir/qtile/color_themes.py ~/.config/qtile/color_themes.py
-ln -s $dir/qtile/autostart.sh ~/.config/qtile/autostart.sh
+ln -s $dir/qtile ~/.config/qtile
 
 # Install picom configuration
 ln -s $dir/picom.conf ~/.config/picom.conf
