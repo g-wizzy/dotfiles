58212971cc982fe026821aa20106e225ad42387e
g-wizzy <pierre.burki@protonmail.com>
2024-08-27 18:03:47 +0200

    Add numlockx to qtile startup

--- home/.config/qtile/autostart.sh
+++ home/.config/qtile/autostart.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-# Disable HDMI output
-xrandr --output HDMI-0 --off
-
-# Picom for transparency
+numlockx
 picom -b
-
-# xscreensaver (so that it can be called with xscreensaver-command -l)
 xscreensaver --no-splash &
-
-# Udiskie (no notifications, tray icon)
 udiskie -N -t &
-
-# Network applet
 nm-applet &
-
-# KDEConnect applet
 kdeconnect-indicator &
-
-# Redshift
 redshift-gtk &
